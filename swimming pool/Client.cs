2a44b0a2d6aff385f8f48cb995bd0da081713939
user1 <rut7723@gmail.com>
2024-11-05 16:33:39 +0200

    the first project

---
+++ swimming pool/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace swimming_pool
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+    }
+}
